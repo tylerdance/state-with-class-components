d1e40b337f8b7e66eb51a13eb2149ccc56ec6dbc
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-12-09 10:25:26 -0500

    counter working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+   this.state = {
+     count: 0
+   }
+
+   this.increment = this.increment.bind(this)
+  }
+
+  increment = () =>  {
+    const currentCount = this.state.count
+    const newCount = currentCount + 1
+
+    this.setState({ count: newCount })
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>+</button>
+      </div>
+    )
+  }
 }
 
 export default App;
