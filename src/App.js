b1300d59460a32b08c371f0dfd77e255137065cf
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-12-09 11:36:32 -0500

    counter with input field

--- src/App.js
+++ src/App.js
@@ -7,33 +7,41 @@ class App extends Component {
     super()
 
    this.state = {
-     count: 0
+     count: 0,
+     inputtedNumber: 0
    }
 
-   this.increment = this.increment.bind(this)
+  //  this.increment = this.increment.bind(this)
   }
 
   increment = () =>  {
-
-    const amountToAdd = prompt('How much do you want to add?')
+    // const amountToAdd = prompt('How much do you want to add?')
     // const currentCount = this.state.count
-    const newCount = this.state.count + parseInt(amountToAdd)
+    const newCount = this.state.count + this.state.inputtedNumber
 
     this.setState({ count: newCount })
   }
 
   decrement = () => {
-    const amountToSubtract = prompt('How much do you want to subtract?')
+    // const amountToSubtract = prompt('How much do you want to subtract?')
     // const currentCount = this.state.count
-    const newCount = this.state.count - parseInt(amountToSubtract)
+    const newCount = this.state.count - this.state.inputtedNumber
 
     this.setState({ count: newCount })
+  }
+
+  changeInput = (e) => {
+    console.log(e.target.value);
+    let newValue = parseInt(e.target.value) || ''
+    this.setState({inputtedNumber: newValue})
+
   }
 
   render() {
     return(
       <div>
         <h1>{this.state.count}</h1>
+        <input type="number" value={this.state.inputtedNumber} onChange={this.changeInput}/>
         <button onClick={this.increment}>+</button>
         <button onClick={this.decrement}>-</button>
       </div>
